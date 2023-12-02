173dc20d283479273c45dba9f1e3f9d408bf2d89
GozzAgu <103033742+GozzAgu@users.noreply.github.com>
2023-12-02 11:53:01 +0100

    .

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -3,7 +3,7 @@
     <img class="w-full h-80 md:h-[40rem] lg:h-screen" src="/images/kuo-hero.webp"/>
     <div class="bg-black h-full absolute inset-0 opacity-75"></div>
     <div class="absolute inset-0 flex flex-row p-4 lg:px-40 lg:py-4 items-center">
-      <p class="text-white text-xl md:text-3xl lg:text-5xl font-bold">Balancing the Equation: Meeting Demand and Supply with Precision and Expertise.</p>
+      <p class="text-white text-3xl lg:text-5xl font-bold">Balancing the Equation: Meeting Demand and Supply with Precision and Expertise.</p>
     </div>
   </div>
 </template>
