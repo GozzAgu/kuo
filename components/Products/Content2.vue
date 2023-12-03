0c8ee0c24c7a1bcf2d09188a789956ff7aec5e34
GozzAgu <103033742+GozzAgu@users.noreply.github.com>
2023-12-03 06:12:33 +0100

    update

--- components/Products/Content2.vue
+++ components/Products/Content2.vue
@@ -3,38 +3,47 @@ const positions = ref([
   {
     name: 'Engineering Services',
     description: '',
+    img: '/images/emmanuel-ikwuegbu--0-kl1BjvFc-unsplash.jpg'
   },
   {
     name: 'LV/MV Electrical System Installation',
     description: '',
+    img: '/images/LM installation.jpg'
   },
   {
     name: 'Fire & Gas Systems Installation',
     description: '',
+    img: '/images/fireandgas.jpg'
   },
   {
     name: 'Oil & Gas Metering Services',
     description: '',
+    img: '/images/oilandgasmetering.jpg'
   },
   {
     name: 'HVAC Systems',
     description: '',
+    img: '/images/hvac.jpg'
   },
   {
     name: 'Valves & Actuation (Installation & Maintenance)',
     description: '',
+    img: '/images/valve.jpg'
   },
   {
     name: 'Procurement',
     description: '',
+    img: '/images/pro.jpg=ws1280x960'
   },
   {
     name: 'Environmental Services',
     description: '',
+    img: '/images/envservices.jpg'
   },
   {
     name: 'Logistics',
     description: '',
+    img: '/images/logistics.jpg'
   },
 ])
 
@@ -85,18 +94,27 @@ onMounted(() => {
         <h1 class="mt-8 text-2xl mb-12 text-blue-900 font-semibold md:border-l-4 border-red-700 pl-2">
           Our Services<Icon class="ml-2" name="ic:outline-double-arrow" />
         </h1>
-        <div class="mt-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div class="mt-8 grid sm:grid-cols-2 md:grid-cols-3 gap-8">
           <div v-for="position in positions">
-            <div class="h-[7em] bg-gradient-to-b from-white to-gray-400 opacity-80 p-4">
-              <p class="text-blue-950 font-bold mb-2 h-[3em]">
+            <div>
+              <img class="h-[12em] w-[100%]" :src="position.img" />
+            </div>
+            <div>
+              <p class="text-white font-bold flex justify-center items-center text-center py-2 h-[4em] bg-blue-950">
                 {{ position.name }}
               </p>
             </div>
+            <!-- <div class="h-[15em] relative bg-gradient-to-b from-white to-gray-400 opacity-80 p-4">
+              <p class="text-blue-950 font-bold mb-2 h-[3em]">
+                {{ position.name }}
+              </p>
+              <img class="absolute" src="/images/PHOTO-2023-07-10-07-22-42.jpg" />
+            </div> -->
           </div>
         </div>
       </div>
 
-      <div class="px-8 py-12 md:py-24 xl:p-24">
+      <!-- <div class="px-8 py-12 md:py-24 xl:p-24">
         <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
           <img class="" src="/images/PHOTO-2023-07-10-07-22-42.jpg">          <img src="/images/PHOTO-2023-07-10-07-22-43.jpg">
           <img class="" src="/images/PHOTO-2023-07-10-07-22-44 (1).jpg">
@@ -105,7 +123,7 @@ onMounted(() => {
           <img class="md:h-[15em] w-full" src="/images/IMG_3723.jpg">
           <img class="md:h-[15em]" src="/images/PHOTO-2023-07-10-07-22-43 (1).jpg">
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
