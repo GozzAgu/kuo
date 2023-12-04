f767ab198fa27b45407eb47f01e5453b85ee4e78
GozzAgu <103033742+GozzAgu@users.noreply.github.com>
2023-12-04 18:56:03 +0100

    commented auth

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, type Auth } from "firebase/auth"
+// import { getAuth, type Auth } from "firebase/auth"
 import { getFirestore } from 'firebase/firestore'
 
 export default defineNuxtPlugin(nuxtApp => {
@@ -18,11 +18,11 @@ export default defineNuxtPlugin(nuxtApp => {
   const app = initializeApp(firebaseConfig)
 
   console.log(app)
-  const auth = getAuth(app)
+  // const auth = getAuth(app)
   const firestore = getFirestore(app)
 
-  nuxtApp.vueApp.provide('auth', auth)
-  nuxtApp.provide('auth', auth)
+  // nuxtApp.vueApp.provide('auth', auth)
+  // nuxtApp.provide('auth', auth)
 
   nuxtApp.vueApp.provide('firestore', firestore)
   nuxtApp.provide('firestore', firestore)
