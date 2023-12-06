e6926464983e3d084948e7ee4b08afeb8e7a1490
GozzAgu <103033742+GozzAgu@users.noreply.github.com>
2023-12-06 10:59:39 +0100

    ...

--- components/Contact/ContactForm.vue
+++ components/Contact/ContactForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="lg:px-40 p-4 lg:py-4">
     <div class="shadow-md bg-white md:rounded-3xl md:flex xl:flex gap-x-16 justify-between py-8 p-4 sm:p-12 mb-24">
-      <div class="md:w-[50%]">
+      <!-- <div class="md:w-[50%]">
         <h1 class="text-xl mb-4 text-blue-900">
           Contact Us
         </h1>
@@ -35,7 +35,96 @@
         >
           Submit
         </button>
-      </div>
+      </div> -->
+
+      <form class="md:w-[50%]" action="https://api.web3forms.com/submit" method="POST">
+        <div class="flex bg-gray-50">
+          <div class="container mx-auto">
+            <div class="max-w-md mx-auto my-10 bg-white p-5 rounded-md shadow-sm">
+              <div class="text-center">
+                <h1
+                  class="my-3 text-3xl font-semibold text-gray-700"
+                >
+                  Contact Us
+                </h1>
+                <p class="text-gray-400 dark:text-gray-400">
+                  Fill up the form below to send us a message.
+                </p>
+              </div>
+              <div class="m-7">
+                <form action="https://api.web3forms.com/submit" method="POST" id="form">
+                  <input type="hidden" name="access_key" value="986d1098-4866-4d24-8633-bf3b0fc77f28" />
+                  <input
+                    type="hidden"
+                    name="subject"
+                    value="Contact form submission"
+                  />
+                  <input
+                    type="hidden"
+                    name="redirect"
+                    value=""
+                  />
+                  <div class="mb-6">
+                    <label
+                      for="name"
+                      class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                      >Full Name</label
+                    >
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      placeholder="John Doe"
+                      required
+                      class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                    />
+                  </div>
+                  <div class="mb-6">
+                    <label
+                      for="email"
+                      class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                      >Email Address</label
+                    >
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="you@company.com"
+                      required
+                      class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                    />
+                  </div>
+                  <div class="mb-6">
+                    <label
+                      for="message"
+                      class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                      >Your Message</label
+                    >
+
+                    <textarea
+                      rows="5"
+                      name="message"
+                      id="message"
+                      placeholder="Your Message"
+                      class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                      required
+                    ></textarea>
+                  </div>
+                  <div class="mb-6">
+                    <button
+                      type="submit"
+                      class="w-full px-3 py-4 text-white bg-blue-900 rounded-md focus:bg-blue-950 focus:outline-none"
+                    >
+                      Send Message
+                    </button>
+                  </div>
+                  <p class="text-base text-center text-gray-400" id="result"></p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
 
       <div class="md:w-[50%]">
         <div class="mb-8">
@@ -51,7 +140,8 @@
             Email Address
           </h1>
           <div class="grid grid-cols-1">
-            <a href="mailto:portharcourt@cyphercrescent.com" class="text-sm hover:underline hover:cursor-pointer">kuosmartsolutions@gmail.com</a>
+            <a href="mailto:info@kuosmartsolutions.com" class="text-sm hover:underline hover:cursor-pointer">info@kuosmartsolutions.com</a>
+            <a href="mailto:sales@kuosmartsolutions.com" class="text-sm hover:underline hover:cursor-pointer">sales@kuosmartsolutions.com</a>
           </div>
         </div>
         <div>
