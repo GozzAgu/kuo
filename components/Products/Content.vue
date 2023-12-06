bcc631c3ca4fd6f7ced38f054a862b315e0c5ca0
GozzAgu <103033742+GozzAgu@users.noreply.github.com>
2023-12-06 07:42:10 +0100

    ...

--- components/Products/Content.vue
+++ components/Products/Content.vue
@@ -122,13 +122,13 @@ const showChild = () => {
 
         <Transition>
           <div v-if="child" class="mt-8 grid md:grid-cols-3 xl:grid-cols-4 gap-4">
-            <div class="flex gap-8 border-2 rounded-lg p-4" v-for="solution in solutions" :key="solution" >
+            <div class="flex gap-8 border-2 rounded-lg p-4 h-[7.5em]" v-for="solution in solutions" :key="solution" >
               <img class="w-[3em] h-[3em] lg:w-[3.5em] lg:h-[3.5em] shrink-0" :src="solution.img" />
-              <div class="line-clamp-1">
+              <div class="relative line-clamp-1">
                 <p class="flex items-center truncate text-sm md:text-base">{{ solution.name }}</p>
                 <hr class="my-2"> 
-                <p class="text-xs line-clamp-1">{{ solution.description }}</p>
-                <p class=" text-xs text-gray-400">MOQ: 1,000 Pieces</p>
+                <p class="text-xs line-clamp-2">{{ solution.description }}</p>
+                <p class="absolute bottom-0 text-xs text-gray-400">MOQ: 1,000 Pieces</p>
               </div>
             </div>        
           </div>
