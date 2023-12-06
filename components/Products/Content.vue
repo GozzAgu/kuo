e7e31a05fa90c56278123986b9cb5c6414e76364
GozzAgu <103033742+GozzAgu@users.noreply.github.com>
2023-12-06 07:36:50 +0100

    ...

--- components/Products/Content.vue
+++ components/Products/Content.vue
@@ -125,7 +125,8 @@ const showChild = () => {
             <div class="flex gap-8 border-2 rounded-lg p-4" v-for="solution in solutions" :key="solution" >
               <img class="w-[3em] h-[3em] lg:w-[3.5em] lg:h-[3.5em] shrink-0" :src="solution.img" />
               <div class="line-clamp-1">
-                <p class="flex items-center truncate text-sm md:text-base">{{ solution.name }}</p> 
+                <p class="flex items-center truncate text-sm md:text-base">{{ solution.name }}</p>
+                <hr class="my-2"> 
                 <p class="text-xs line-clamp-2">{{ solution.description }}</p>
                 <p class=" text-xs text-gray-400">MOQ: 1,000 Pieces</p>
               </div>
